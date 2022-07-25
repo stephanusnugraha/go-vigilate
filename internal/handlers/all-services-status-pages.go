46890b60855432ab807c2b2beb3166e678e000c3
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-07-25 16:56:36 +0700

    list all services by status

--- internal/handlers/all-services-status-pages.go
+++ internal/handlers/all-services-status-pages.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"github.com/CloudyKit/jet/v6"
 	"github.com/tsawler/vigilate/internal/helpers"
+	"log"
 	"net/http"
 )
 
@@ -31,7 +33,16 @@ func (repo *DBRepo) AllProblemServices(w http.ResponseWriter, r *http.Request) {
 
 // AllPendingServices lists all pending services
 func (repo *DBRepo) AllPendingServices(w http.ResponseWriter, r *http.Request) {
-	err := helpers.RenderPage(w, r, "pending", nil, nil)
+	// get all host services (with host info) for status pending
+	services, err := repo.DB.GetServicesByStatus("pending")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	vars := make(jet.VarMap)
+	vars.Set("services", services)
+
+	err = helpers.RenderPage(w, r, "pending", vars, nil)
 	if err != nil {
 		printTemplateError(w, err)
 	}
