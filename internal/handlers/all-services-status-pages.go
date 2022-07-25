8777572d2abc6586b6d75cb258d84548b9401dea
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-07-25 21:37:27 +0700

    add list services for healthy, warning and problems page

--- internal/handlers/all-services-status-pages.go
+++ internal/handlers/all-services-status-pages.go
@@ -9,7 +9,15 @@ import (
 
 // AllHealthyServices lists all healthy services
 func (repo *DBRepo) AllHealthyServices(w http.ResponseWriter, r *http.Request) {
-	err := helpers.RenderPage(w, r, "healthy", nil, nil)
+	services, err := repo.DB.GetServicesByStatus("healthy")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	vars := make(jet.VarMap)
+	vars.Set("services", services)
+
+	err = helpers.RenderPage(w, r, "healthy", vars, nil)
 	if err != nil {
 		printTemplateError(w, err)
 	}
@@ -17,7 +25,15 @@ func (repo *DBRepo) AllHealthyServices(w http.ResponseWriter, r *http.Request) {
 
 // AllWarningServices lists all warning services
 func (repo *DBRepo) AllWarningServices(w http.ResponseWriter, r *http.Request) {
-	err := helpers.RenderPage(w, r, "warning", nil, nil)
+	services, err := repo.DB.GetServicesByStatus("warning")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	vars := make(jet.VarMap)
+	vars.Set("services", services)
+
+	err = helpers.RenderPage(w, r, "warning", vars, nil)
 	if err != nil {
 		printTemplateError(w, err)
 	}
@@ -25,7 +41,15 @@ func (repo *DBRepo) AllWarningServices(w http.ResponseWriter, r *http.Request) {
 
 // AllProblemServices lists all problem services
 func (repo *DBRepo) AllProblemServices(w http.ResponseWriter, r *http.Request) {
-	err := helpers.RenderPage(w, r, "problems", nil, nil)
+	services, err := repo.DB.GetServicesByStatus("problems")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	vars := make(jet.VarMap)
+	vars.Set("services", services)
+
+	err = helpers.RenderPage(w, r, "problems", vars, nil)
 	if err != nil {
 		printTemplateError(w, err)
 	}
