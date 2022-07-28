6acf87e3e529f83f2bf1022110af55b8fd715200
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-07-28 10:51:37 +0700

    push monitoring start/stop events to client

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -22,8 +22,6 @@ func routes() http.Handler {
 
 	mux.Get("/user/logout", handlers.Repo.Logout)
 
-	mux.Get("/pusher-test", handlers.Repo.TestPusher)
-
 	mux.Route("/pusher", func(mux chi.Router) {
 		mux.Use(Auth)
 		mux.Post("/auth", handlers.Repo.PusherAuth)
